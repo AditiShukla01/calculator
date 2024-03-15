b101a5e9076ebd152b1894fa005158758a4cac19
Aditi Shukla <156644431+AditiShukla01@users.noreply.github.com>
2024-03-15 18:40:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+        function insert(num) {
+            document.getElementById('textview').value = document.getElementById('textview').value+num;
+        }
+        function clean() {
+            document.getElementById('textview').value = "";
+        }
+        function back() {
+            var value = document.getElementById('textview').value;
+            document.getElementById('textview').value = value.substring(0, value.length-1);
+        }
+        function result() {
+            var value = document.getElementById('textview').value;
+            if(value) {
+                document.getElementById('textview').value = eval(value);
+            }
+        }
